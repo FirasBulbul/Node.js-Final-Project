d71e58112adae359a7f383625bda5af2694b394a
mahmoud-abu-sultan <mahmoudabosultan.t@gmail.com>
2023-05-19 22:44:07 +0300

    handling the data in just to json file (categories.json & news.json)

--- controllers/BeinSports_Scraping/descCarsNews.js
+++ controllers/BeinSports_Scraping/descCarsNews.js
@@ -1,86 +1,82 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
 const fs = require("fs");
+//---
 
 const getDescNewsinfo = async () => {
-  const main_categorie = "Motorsports"; //--- get from json us front
-  // Football Tennis Basketball Cricket Motorsports NBA
-
-  const descriptionLink = //I--- get from json us front
-    //test // "https://www.beinsports.com/en/uefa-europa-league/video/gatti-snatches-juve-late-draw-against-sevilla/2088789";//f v
-    //test // "https://www.beinsports.com/en/tennis/news/tsitsipas-wins-after-long-wait-in-miami/2062412";//t i
-    //test // "https://www.beinsports.com/en/nba/video/nba-round-up-lakers-crush-grizzlies-to-advanc/2079721"; //n v
-    "https://www.beinsports.com/en/f1/news/red-bulls-perez-wins-azerbaijan-grand-prix-sp/2079909"; //m I
-
-  const htmlContent = await request.get(descriptionLink);
-  // ---
-  const readFileJson = fs.readFileSync(
-    `./data/${main_categorie}Data.json`,
+  const readFileNews_json = await fs.readFileSync(
+    "../../data/news.json",
     "utf8"
   );
-  const dataJson = await JSON.parse(readFileJson);
+  const dataListNews = await JSON.parse(readFileNews_json);
 
-  const hasVideo = false; /*dataJson.find(
-    (ele) => ele.descriptionLink === descriptionLink
-  ).has_video;*/
+  for (var index in dataListNews) {
+    const thisOpj = dataListNews[index];
 
-  if (htmlContent) {
-    const $ = cheerio.load(htmlContent);
+    const descriptionLink = thisOpj.descriptionLink;
+    const htmlContent = await request.get(descriptionLink);
+    // ---
 
-    //// titles
-    const selector_titles = () => {
-      if (hasVideo) {
-        return ".video h1";
-      } else {
-        return "body > section.page-story > div > div > div.block-col.block-flex-2-columns > main > article > div > article > header > h1";
+    if (htmlContent) {
+      const $ = cheerio.load(htmlContent);
+
+      const hasVideo = dataListNews[index].has_video;
+      console.log(hasVideo);
+
+      // titles
+      const selector_titles = () => {
+        if (hasVideo) {
+          return ".video h1";
+        } else {
+          return "body > section.page-story > div > div > div.block-col.block-flex-2-columns > main > article > div > article > header > h1";
+        }
+      };
+      const title = $(`${selector_titles()}`).text().trim();
+
+      //// video_link
+      const videoLink = $("#f198c86f9f22bf4").attr("src");
+
+      // article_body
+      const selector_article_body = () => {
+        if (hasVideo) {
+          return "#main_gallery > div > div > div > div > article > section > div:nth-child(1)";
+        } else {
+          return "body > section.page-story > div > div > div.block-col.block-flex-2-columns > main > article > div > article > div.plrm > section > div:nth-child(1)";
+        }
+      };
+      const articleBody = $(`${selector_article_body()}`).text().trim();
+
+      // datePublished
+      const datePublished = $(".main_article__author time").text().trim();
+
+      // --- ---
+      if (thisOpj != undefined) {
+        if (thisOpj.title == "" || thisOpj.title == undefined) {
+          thisOpj.title = title;
+          thisOpj.articleBody = articleBody;
+        }
+        thisOpj.datePublished = datePublished;
+        thisOpj.videoLink = videoLink;
+
+        await fs.writeFileSync(
+          `../../data/news.json`,
+          JSON.stringify(dataListNews)
+        );
       }
-    };
-    const title = $(`${selector_titles()}`).text().trim();
-
-    //// datePublished
-    const datePublished = $(".main_article__author time").text().trim();
-
-    //// video_link
-    const videoLink = $("#f198c86f9f22bf4").attr("src");
-
-    //// article_body
-    const selector_article_body = () => {
-      if (hasVideo) {
-        return "#main_gallery > div > div > div > div > article > section > div:nth-child(1)";
-      } else {
-        return "body > section.page-story > div > div > div.block-col.block-flex-2-columns > main > article > div > article > div.plrm > section > div:nth-child(1)";
-      }
-    };
-    const articleBody = $(`./data/${selector_article_body()}`).text().trim();
-
-    // --- ---
-    const idOpj = "69001";
-    const thisOpj = dataJson.find((ele) => ele.id == idOpj);
-
-    if (thisOpj != undefined) {
-      thisOpj.title = title;
-      thisOpj.datePublished = datePublished;
-      thisOpj.videoLink = videoLink;
-      thisOpj.articleBody = articleBody;
-
-      await fs.writeFileSync(
-        `./data/${main_categorie}Data.json`,
-        JSON.stringify(dataJson)
-      );
-    }
-  } else {
-    throw new console.error({
-      message: "Error in Internet conniction - DescCarsNews",
-    });
-  }
+    } else {
+      throw new console.error({
+        message: "Error in Internet conniction - DescCarsNews",
+      });
+    } //end if-condtion for get data for it's categorie
+  } //END main loop for all categories
 };
 
-module.exports = getDescNewsinfo;
+// module.exports = getDescNewsinfo;
 
-// getDescNewsinfo()
-//   .then((data) => {
-//     console.log("OK: 200 - DescCarsNews");
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
+getDescNewsinfo()
+  .then((data) => {
+    console.log("OK: 200 - DescCarsNews");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
