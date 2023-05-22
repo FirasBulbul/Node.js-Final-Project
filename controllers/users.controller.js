568063b3bfde66cf3de6bb56f9dc9c418bd23854
Firas Abdulrahman <106191320+FirasBulbul@users.noreply.github.com>
2023-05-22 18:23:24 +0300

    ...

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -6,6 +6,38 @@ const jwt = require("jsonwebtoken");
 
 const usersModel = require("../models/users.model");
 
+//signUp ---
+const signUp = async (data) => {
+    try {
+        const { first_name, last_name, email, password } = data;
+        const oldUser = await usersModel.findOne({ email });
+        if (oldUser) {
+            return { error: "User Is Exists. Please Login" };
+        }
+        // Encrypting Password
+        const encryptedPassword = await bcrypt.hash(password, 15);
+
+        const user = await usersModel.create({
+            first_name: first_name,
+            last_name: last_name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+            created_at: new Date(),
+        });
+        // construct token
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN,
+        );
+        // save token
+        user.token = token;
+        // new user
+        return user;
+    } catch (error) {
+        console.log("Couldn't Create a New User", error);
+    }
+};
+
 //register ---
 const register = async (request, response, next) => {
     try {
@@ -25,6 +57,33 @@ const register = async (request, response, next) => {
         return response.status(200).json(createdUser);
     } catch (error) {
         return response.status(500).json({ error: error });
+    }
+};
+
+//signIn ---
+const signIn = async () => {
+    try {
+        const { email, password } = data;
+        const user = await usersModel.findOne({ email });
+        if (!user) {
+            return { error: "Join us :)" };
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return { error: "Error Username or Password: Please Check them :)" };
+        }
+        // costruct a token
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN,
+        );
+        // save token
+        user.token = token;
+        // logging user
+        return user;
+    } catch (error) {
+        console.log("Sign In Error", error);
     }
 };
 
